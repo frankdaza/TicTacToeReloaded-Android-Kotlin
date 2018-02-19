d3253c7bcb9295eae6841d82ffe0f514b8b731de
Frank Edward Daza González <frank.daza2@gmail.com>
2018-02-18 22:32:16 -0500

    Created the function that check the winner, redirect to the winner activity and clear the top of activities from the stack and return to the main activity. Bug fixes from playMove function of the GameBoardActivity.

--- app/src/main/java/com/frankdaza/tictactoereloaded/GameBoardActivity.kt
+++ app/src/main/java/com/frankdaza/tictactoereloaded/GameBoardActivity.kt
@@ -1,7 +1,9 @@
 package com.frankdaza.tictactoereloaded
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -11,6 +13,9 @@ class GameBoardActivity : AppCompatActivity() {
 
     private var typeOfGame: Int = 1
     private var move: String = "X"
+    private var listMovesPlayer1: ArrayList<Int> = ArrayList<Int>()
+    private var listMovesPlayer2: ArrayList<Int> = ArrayList<Int>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,23 +40,39 @@ class GameBoardActivity : AppCompatActivity() {
         if (buttonSelected.isEnabled) {
             buttonSelected.text = this.move
             buttonSelected.isEnabled = false
+            var buttonNewId: Int = 0
+
+            when (buttonSelected.id) {
+                R.id.button1 -> buttonNewId = 1
+                R.id.button2 -> buttonNewId = 2
+                R.id.button3 -> buttonNewId = 3
+                R.id.button4 -> buttonNewId = 4
+                R.id.button5 -> buttonNewId = 5
+                R.id.button6 -> buttonNewId = 6
+                R.id.button7 -> buttonNewId = 7
+                R.id.button8 -> buttonNewId = 8
+                R.id.button9 -> buttonNewId = 9
+            }
 
             if (this.typeOfGame == 1) {
                 Toast.makeText(view.context, "Missing this functionality", Toast.LENGTH_LONG).show()
             } else {
                 if (this.move == "X") {
+                    this.listMovesPlayer1.add(buttonNewId)
                     this.move = "O"
-                    turnOfPlayer(view)
                 } else {
+                    this.listMovesPlayer2.add(buttonNewId)
                     this.move = "X"
-                    turnOfPlayer(view)
                 }
             }
+
+            checkWinner(view)
+            turnOfPlayer(view)
         }
     }
 
     /**
-     * Set the turn of player
+     * Set the turn of the player
      *
      * Created by Frank Edward Daza González on Feb 18, 2018
      */
@@ -60,6 +81,48 @@ class GameBoardActivity : AppCompatActivity() {
             tvPlayer.text = getString(R.string.lbl_turn_player_1)
         } else {
             tvPlayer.text = getString(R.string.lbl_turn_player_2)
+        }
+    }
+
+    /**
+     * Check the winner of the game
+     *
+     * Created by Frank Edward Daza González on Feb 18, 2018
+     */
+    fun checkWinner(view: View) {
+        if (this.typeOfGame == 1) {
+
+        } else {
+            // Check if the player 1 won
+            if (this.listMovesPlayer1.contains(1) && this.listMovesPlayer1.contains(2) && this.listMovesPlayer1.contains(3)
+                    || this.listMovesPlayer1.contains(4) && this.listMovesPlayer1.contains(5) && this.listMovesPlayer1.contains(6)
+                    || this.listMovesPlayer1.contains(7) && this.listMovesPlayer1.contains(8) && this.listMovesPlayer1.contains(9)
+                    || this.listMovesPlayer1.contains(1) && this.listMovesPlayer1.contains(4) && this.listMovesPlayer1.contains(7)
+                    || this.listMovesPlayer1.contains(2) && this.listMovesPlayer1.contains(5) && this.listMovesPlayer1.contains(8)
+                    || this.listMovesPlayer1.contains(3) && this.listMovesPlayer1.contains(6) && this.listMovesPlayer1.contains(9)
+                    || this.listMovesPlayer1.contains(1) && this.listMovesPlayer1.contains(5) && this.listMovesPlayer1.contains(9)
+                    || this.listMovesPlayer1.contains(3) && this.listMovesPlayer1.contains(5) && this.listMovesPlayer1.contains(7)) {
+
+                val intent = Intent(view.context, WinnerActivity::class.java)
+                intent.putExtra("winner", 1)
+                ContextCompat.startActivity(view.context, intent, null)
+            }
+
+            // Check if the player 2 won
+            if (this.listMovesPlayer2.contains(1) && this.listMovesPlayer2.contains(2) && this.listMovesPlayer2.contains(3)
+                    || this.listMovesPlayer2.contains(4) && this.listMovesPlayer2.contains(5) && this.listMovesPlayer2.contains(6)
+                    || this.listMovesPlayer2.contains(7) && this.listMovesPlayer2.contains(8) && this.listMovesPlayer2.contains(9)
+                    || this.listMovesPlayer2.contains(1) && this.listMovesPlayer2.contains(4) && this.listMovesPlayer2.contains(7)
+                    || this.listMovesPlayer2.contains(2) && this.listMovesPlayer2.contains(5) && this.listMovesPlayer2.contains(8)
+                    || this.listMovesPlayer2.contains(3) && this.listMovesPlayer2.contains(6) && this.listMovesPlayer2.contains(9)
+                    || this.listMovesPlayer2.contains(1) && this.listMovesPlayer2.contains(5) && this.listMovesPlayer2.contains(9)
+                    || this.listMovesPlayer2.contains(3) && this.listMovesPlayer2.contains(5) && this.listMovesPlayer2.contains(7)) {
+
+                val intent = Intent(view.context, WinnerActivity::class.java)
+                intent.putExtra("winner", 2)
+                ContextCompat.startActivity(view.context, intent, null)
+            }
+
         }
     }
 
---
+++ app/src/main/java/com/frankdaza/tictactoereloaded/WinnerActivity.kt
@@ -0,0 +1,36 @@
+package com.frankdaza.tictactoereloaded
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.view.View
+import kotlinx.android.synthetic.main.activity_game_board.*
+
+class WinnerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_winner)
+
+        // Get the winner of the game
+        val bundle: Bundle = intent.extras
+        var winner: Int = bundle.getInt("winner")
+
+        if (winner == 1)
+            tvPlayer.text = getString(R.string.lbl_player1_wins)
+        else
+            tvPlayer.text = getString(R.string.lbl_player2_wins)
+    }
+
+    /**
+     * Clear all the activities from the stack and redirect to the main activity
+     *
+     * Created by Frank Edward Daza González on Feb 18, 2018
+     */
+    fun playAgain(view: View) {
+        val intent = Intent(view.context, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        ContextCompat.startActivity(view.context, intent, null)
+    }
+}
